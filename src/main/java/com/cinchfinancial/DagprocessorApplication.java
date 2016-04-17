dcc2689021b04fa3a7846a3257c21522320bd899
Jay Bhambhani <jay@pixability.com>
2016-04-16 21:01:27 -0400

    first push

---
+++ src/main/java/com/cinchfinancial/DagprocessorApplication.java
@@ -0,0 +1,12 @@
+package com.cinchfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagprocessorApplication.class, args);
+	}
+}
